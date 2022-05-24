de58731b7b54f8b06a8280cff561712635889fea
Méndez <47764656+El-Mendez@users.noreply.github.com>
2022-05-24 14:30:39 -0600

    Random oh-my-posh themes

--- pwsh_profile.ps1
+++ pwsh_profile.ps1
@@ -1,2 +1,15 @@
-oh-my-posh --init --shell pwsh --config "~\AppData\Local\Programs\oh-my-posh\themes\spaceship.omp.json" | Invoke-Expression
+$themes = @(
+	"amro", "atomic", "avit", "blueish", "blue-owl", "craver", "di4am0nd", "emodipt",
+	"festivetech", "fish", "half-life", "honukai", "hunk", "iterm2", "jblab_2021", 
+	"material", "microverse-power", "montys", "night-owl", "powerline", "probua.minimal",
+	"sonicboom_dark", "sorin", "space", "spaceship", "unicorn", "ys"
+)
+
+$CURR_THEME = Get-Random $themes
+Remove-Variable themes
+
+echo "[oh-my-posh] Random theme '$CURR_THEME' loaded"
+echo ""
+
+oh-my-posh --init --shell pwsh --config "$env:POSH_THEMES_PATH\$CURR_THEME.omp.json" | Invoke-Expression
 Set-PSReadLineOption -PredictionSource History
