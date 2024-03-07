b2072776b7ce2a1759f6ce9fdf408ba0ff8f0597
Méndez <47764656+El-Mendez@users.noreply.github.com>
2024-03-06 18:11:46 -0600

    add react-native env variables

--- shells/zsh/desktop.zsh
+++ shells/zsh/desktop.zsh
@@ -18,10 +18,17 @@ if [[ $ZSH_EVAL_CONTEXT == 'file' ]]; then
     source $ZSH/oh-my-zsh.sh
 fi
 
-export ANDROID_HOME="~/Android/Sdk"
 alias s="kitty +kitten ssh"
 source ~/.config/op/plugins.sh
 
-alias adb="~/Android/Sdk/platform-tools/adb"
-alias emulator="~/Android/Sdk/tools/emulator"
+# For Android Development
+export ANDROID_HOME="/home/mendez/Android/Sdk"
+export JAVA_HOME="/home/mendez/.local/share/JetBrains/Toolbox/apps/intellij-idea-ultimate/jbr"
+export ANDROID_PLATFORM_TOOLS="/home/mendez/Android/Sdk/platform-tools"
+export JBR="/home/mendez/.local/share/JetBrains/Toolbox/apps/android-studio/jbr/bin"
+export ANDROID_CMD_TOOLS="/home/mendez/Android/Sdk/cmdline-tools/latest/bin"
+export ANDROID_EMULATOR="/home/mendez/Android/Sdk/emulator"
+
+export PATH=$JBR:$PATH:$ANDROID_PLATFORM_TOOLS:$ANDROID_CMD_TOOLS:$ANDROID_EMULATOR
+
 
