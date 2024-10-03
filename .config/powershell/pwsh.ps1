a1381ab90d1f6c75f6053b500bbc8d1c8524e68a
Méndez <47764656+El-Mendez@users.noreply.github.com>
2024-10-03 00:41:17 -0600

    fix windows config

--- .config/powershell/pwsh.ps1
+++ .config/powershell/pwsh.ps1
@@ -1,14 +1,3 @@
-$themes = @(
-	"amro", "di4m0nd", "easy-term", "emodipt", "emodipt-extend", "honukai", "powerlevel10k_lean", 
-        "probua.minimal", "pure", "spaceship", "star", "the-unnamed", "wopian", "ys", "zash"
-)
-
-$CURR_THEME = Get-Random $themes
-Remove-Variable themes
-
-echo "[oh-my-posh] Random theme '$CURR_THEME' loaded"
-echo ""
-
-oh-my-posh --init --shell pwsh --config "$env:POSH_THEMES_PATH\$CURR_THEME.omp.json" | Invoke-Expression
-Import-Module z
+Invoke-Expression (& 'C:\Program Files\starship\bin\starship.exe' init powershell)
+Invoke-Expression (& { (zoxide init powershell | Out-String) })
 New-Alias v nvim
